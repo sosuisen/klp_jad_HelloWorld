7992fbee7d81ebf15d982022c8863dd3460ad560
sosuisen <hidekazu.kubota@gmail.com>
2023-03-17 01:42:14 +0900

    feat: add answer of hatten kadai

---
+++ src/main/java/com/example/MyApp.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MyApp extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		var javaVersion = System.getProperty("java.version");
+		var javafxVersion = System.getProperty("javafx.version");
+
+		var label = new Label("Hello, KCG");
+		// 発展課題：中央揃え
+		label.setStyle("-fx-font-size: 60px; -fx-font-weight: bold; -fx-alignment: center; -fx-pref-width: 640;");
+
+		/* 別解
+		label.setStyle("-fx-font-size: 60px; -fx-font-weight: bold; -fx-alignment: center;");
+		label.setPrefWidth(640);
+		*/
+			
+		var bPane = new BorderPane();
+		// 発展課題
+		bPane.setBottom(label);
+
+		var scene = new Scene(bPane, 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
