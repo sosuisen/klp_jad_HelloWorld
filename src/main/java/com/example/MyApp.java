27b0fda6cf555bbf38f364731868272a66f9ddd7
sosuisen <hidekazu.kubota@gmail.com>
2023-03-22 12:40:40 +0900

    fix: another answer

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -14,22 +14,24 @@ public class MyApp extends Application {
 		var javafxVersion = System.getProperty("javafx.version");
 
 		var label = new Label("Hello, KCG");
-		// 発展課題：中央揃え
+		// 発展課題2b：中央揃え
 		label.setStyle("-fx-font-size: 60px; -fx-font-weight: bold; -fx-alignment: center; -fx-pref-width: 640;");
-
+		
 		/**
-		 * 別解。すべてメソッドで書く
-		 */
-		/*
-		label.setStyle("-fx-font-size: 60px; -fx-font-weight: bold;");
-		label.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR, 60));
+		 * 発展課題2b：中央揃え別解１。すべてメソッドで書く
+		label.setFont(Font.font("", FontWeight.BOLD, FontPosture.REGULAR, 60));
 		label.setAlignment(Pos.CENTER);
 		label.setPrefWidth(640);
-		 */
+		*/
 			
 		var bPane = new BorderPane();
-		// 発展課題
+		// 発展課題2a
 		bPane.setBottom(label);
+		/**
+		 * 発展課題2b：中央揃え別解2。BorderPaneの子要素 label の配置を変更。
+		label.setStyle("-fx-font-size: 60px; -fx-font-weight: bold;");
+		BorderPane.setAlignment(label, Pos.CENTER);
+		*/
 
 		var scene = new Scene(bPane, 640, 480);
 		stage.setScene(scene);
